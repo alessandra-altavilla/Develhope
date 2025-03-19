084f1c7ec10c547a5f2cf6dabdb07368dbd36421
Alessandra <alessandraaltavilla96@gmail.com>
2025-03-19 21:39:27 +0100

    nuovi esercizi array

---
+++ Javascript/Esercizio 8/esercizio4.js
@@ -0,0 +1,9 @@
+const names = ["Luca", "Marco", "Vittorio", "Giovanni"];
+
+names.pop("Giovanni")
+console.log(names)
+names.push("Pippo")
+console.log(names)
+names.unshift("Giovanni")
+console.log(names)
+
