399597b113e524691da3a668ed03051ee07f089e
Alessandra <alessandraaltavilla96@gmail.com>
2025-04-15 14:27:39 +0200

    es2

---
+++ Javascript DOM/es2/esercizio2.js
@@ -0,0 +1,8 @@
+const paragrafi = document.querySelectorAll('.paragrafo');
+console.log('Elementi con classe "paragrafo":', paragrafi);
+
+const elemento3 = document.getElementById('elemento-3');
+console.log('Elemento con id "elemento-3":', elemento3);
+
+const heading2 = document.querySelectorAll('h2');
+console.log('Elementi <h2>:', heading2);
