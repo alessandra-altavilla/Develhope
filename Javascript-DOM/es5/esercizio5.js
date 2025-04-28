ec498a6bb1438535489b0028717d1eeb48313983
Alessandra <alessandraaltavilla96@gmail.com>
2025-04-28 22:56:04 +0200

    fine esercizi dom

--- Javascript-DOM/es5/esercizio5.js
+++ Javascript-DOM/es5/esercizio5.js
@@ -5,4 +5,5 @@ Al click del bottone inserisca la modalità dark*/
 const button = document.querySelector(".btn");
 const body = document.body;
 
-button.addEventListener("click", () => body.classList.add("theme"));
+button.addEventListener("click", () => body.classList.toggle("theme"));
+
