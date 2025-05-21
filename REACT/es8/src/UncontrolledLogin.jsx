9c1ffca328f359c823d13fa55689f69aaff0be52
Alessandra <alessandraaltavilla96@gmail.com>
2025-05-21 16:59:29 +0200

    correzione

--- REACT/es8/src/UncontrolledLogin.jsx
+++ REACT/es8/src/UncontrolledLogin.jsx
@@ -1,12 +1,20 @@
 export default function UncontrolledLogin() {
+  function onLogin(event) {
+    event.preventDefault();
+
+    const username = event.target.form.username.value;
+    const password = event.target.form.password.value;
+
+    console.log({ username, password });
+  }
   function handleLogin(event) {
-    event.preventDefault()
+    event.preventDefault();
 
-    const formData = new FormData(event.target)
-    const username = formData.get("username")
-    const password = formData.get("password")
+    const formData = new FormData(event.target);
+    const username = formData.get("username");
+    const password = formData.get("password");
 
-    console.log({username, password})
+    console.log({ username, password });
   }
 
   return (
@@ -42,6 +50,7 @@ export default function UncontrolledLogin() {
         </div>
         <div>
           <button type="submit">Login!</button>
+          <button onClick={onLogin}>Login2</button>
         </div>
       </div>
     </form>
