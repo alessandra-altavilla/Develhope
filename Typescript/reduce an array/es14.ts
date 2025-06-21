2fdeb03f1f1dc30e21753821076465be739add21
Alessandra <alessandraaltavilla96@gmail.com>
2025-06-21 16:29:19 +0200

    nuovbi esercizi typescripyt

---
+++ Typescript/reduce an array/es14.ts
@@ -0,0 +1,20 @@
+const populations = [
+    1_412_600_000,
+    60_317_116,
+    69_950_850,
+    10_678_632,
+];
+//The Array reduce method allows us to generate a single value from an array of values.
+//Below we're using it to sum the values of the populations array.
+//The population parameter in the callback has an inferred type of number.
+//Change the initial value (false) to a value with the same type as population. This should fix the type error on Line 23.
+
+const initialValue: number = 1
+
+const sumOfPopulations = populations.reduce((total, population) => {
+    return total + population;
+}, initialValue);
+
+console.log(sumOfPopulations);
+
+export {};
